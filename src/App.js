8bb1e1e095003b810d10d09b6fb3a7f695d9bf60
imac <sjalvara@espol.edu.ec>
2019-06-11 09:44:38 -0500

    CHANGE APP TEXT

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Hi there work
+          CAMBIO 
         </p>
         <a
           className="App-link"
